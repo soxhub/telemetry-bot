bff358420d747384b09f49f6bd78f8196b445cd6
Kevin Stenerson <kstenerson@auditboard.com>
2020-06-03 12:34:06 -0600

    Add async and CLI plumbing

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ A telemetry exporter that scrapes Prometheus and Jaeger and saves it
 to a compact Postgres-based time series database.
 
 ![Fact Core](doc/bot.png)
+
+
+
+## Development
+How do I do `X` in this rust project?
+
+ - __surf:__ http client
+ - __sqlx:__ database client
+ - __async-std:__ async IO (files, sockets, etc)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+//! Telemetry Bot
+
+use anyhow::Result; // alias std::result::Result with dynamic error type
+use std::sync::atomic::{AtomicBool, Ordering};
+
+/// A flag to shutdown the bot
+pub static SHOULD_EXIT: AtomicBool = AtomicBool::new(false);
+
+/// Asks the program to exit.
+pub fn shutdown() {
+    SHOULD_EXIT.store(true, Ordering::SeqCst)
+}
+
+/// The program's main entry point.
+fn main() -> Result<()> {
+    // When we receive a SIGINT (or SIGTERM) signal, begin exiting.
+    ctrlc::set_handler(shutdown).expect("Error setting signal handlers.");
+
+    // Start the main event loop
+    async_std::task::block_on(run())
+}
+
+/// The main thread's event loop
+async fn run() -> Result<()> {
+    println!("Beep boop");
+    Ok(())
 }
