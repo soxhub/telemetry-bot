906ce60dded681f483db4483475df286a3acfa25
Kevin Stenerson <kstenerson@auditboard.com>
2020-07-10 14:02:49 -0600

    Encode series keys using zigzag varint deltas

--- storage_standalone/src/lib.rs
+++ storage_standalone/src/lib.rs
@@ -20,11 +20,11 @@ struct LabelValueKey(Spur);
 
 #[repr(transparent)]
 #[derive(Eq, PartialEq, Hash)]
-struct SeriesKey(Box<[(LabelNameKey, LabelValueKey)]>);
+struct SeriesKey(Box<[u8]>);
 
 impl SeriesKey {
     pub fn size_of(&self) -> usize {
-        self.0.len() * std::mem::size_of::<(LabelNameKey, LabelValueKey)>()
+        self.0.len()
     }
 }
 
@@ -83,6 +83,37 @@ impl Connector {
         }
 
         Ok(())
+    }
+
+    fn build_series_key(&self, label_pairs: &Vec<(&str, &str)>) -> SeriesKey {
+        let mut key_builder = encoding::U32ZigZagEncoder::sized_for(label_pairs.len() * 2);
+        debug_assert_eq!(
+            std::mem::size_of::<LabelNameKey>(),
+            std::mem::size_of::<u32>()
+        );
+        debug_assert_eq!(
+            std::mem::size_of::<LabelValueKey>(),
+            std::mem::size_of::<u32>()
+        );
+        for (name, _) in label_pairs {
+            let name_key = LabelNameKey(self.label_names.get_or_intern(name));
+
+            // Safety: Spur is a #[repr(transparent)] wrapper around NonZeroU32
+            let name_id = unsafe { std::mem::transmute::<LabelNameKey, u32>(name_key) };
+
+            // Encode the ids into our series key
+            key_builder.encode(name_id);
+        }
+        for (_, value) in label_pairs {
+            let value_key = LabelValueKey(self.label_values.get_or_intern(value));
+
+            // Safety: Spur is a #[repr(transparent)] wrapper around NonZeroU32
+            let value_id = unsafe { std::mem::transmute::<LabelValueKey, u32>(value_key) };
+
+            // Encode the ids into our series key
+            key_builder.encode(value_id);
+        }
+        SeriesKey(key_builder.finish().into_boxed_slice())
     }
 
     /// Write a batch of samples to the remote write storage backend
@@ -149,16 +180,7 @@ impl Connector {
                 label_pairs.sort_by(|a, b| a.0.cmp(&b.0));
 
                 // Create a comparable + hashable key for the series
-                let label_keys = label_pairs
-                    .iter()
-                    .map(|(name, value)| {
-                        (
-                            LabelNameKey(self.label_names.get_or_intern(name)),
-                            LabelValueKey(self.label_values.get_or_intern(value)),
-                        )
-                    })
-                    .collect::<Vec<_>>();
-                let series_key = SeriesKey(label_keys.into_boxed_slice());
+                let series_key = self.build_series_key(&label_pairs);
 
                 // Find the series id and save the histogram row to be inserted
                 if let Some(series_id) = self.series.get(&series_key) {
@@ -237,16 +259,7 @@ impl Connector {
                 label_pairs.sort_by(|a, b| a.0.cmp(&b.0));
 
                 // Create a comparable + hashable key for the series
-                let label_keys = label_pairs
-                    .iter()
-                    .map(|(name, value)| {
-                        (
-                            LabelNameKey(self.label_names.get_or_intern(name)),
-                            LabelValueKey(self.label_values.get_or_intern(value)),
-                        )
-                    })
-                    .collect::<Vec<_>>();
-                let series_key = SeriesKey(label_keys.into_boxed_slice());
+                let series_key = self.build_series_key(&label_pairs);
 
                 // Find the series id and save the data row to be inserted
                 let timestamp = sample.timestamp.unwrap_or(default_timestamp);
@@ -599,5 +612,188 @@ mod schema {
         conn.execute(finish_migration.as_str()).await?;
 
         Ok(())
+    }
+}
+
+mod encoding {
+    #![allow(dead_code)]
+
+    pub struct U32VarintEncoder {
+        output: Vec<u8>,
+    }
+
+    impl U32VarintEncoder {
+        pub fn sized_for(num_ints: usize) -> Self {
+            Self {
+                // Ballpark the memory we'll need; to avoid excess re-allocations
+                output: Vec::with_capacity(num_ints * 2),
+            }
+        }
+
+        pub fn encode(&mut self, value: u32) {
+            let mut buffer = [0; 5];
+            let encoded = unsigned_varint::encode::u32(value, &mut buffer);
+            self.output.extend_from_slice(encoded);
+        }
+
+        pub fn finish(self) -> Vec<u8> {
+            self.output
+        }
+    }
+
+    pub struct U32VarintDecoder<'a> {
+        input: &'a [u8],
+    }
+
+    impl<'a> U32VarintDecoder<'a> {
+        pub fn new(input: &'a [u8]) -> Self {
+            Self { input }
+        }
+
+        pub fn decode_all(mut self) -> Option<Vec<u32>> {
+            let min_results = 1 + (self.input.len() / 5); // results will contain _at least_ one integer per 5 bytes
+            let mut results = Vec::with_capacity(min_results);
+            while !self.input.is_empty() {
+                results.push(self.decode()?);
+            }
+            Some(results)
+        }
+
+        #[inline]
+        pub fn decode(&mut self) -> Option<u32> {
+            let (head, tail) = unsigned_varint::decode::u32(self.input).ok()?;
+            self.input = tail;
+            Some(head)
+        }
+    }
+
+    pub struct U32ZigZagEncoder {
+        output: Vec<u8>,
+        prev: u32,
+    }
+
+    impl U32ZigZagEncoder {
+        pub fn sized_for(num_ints: usize) -> Self {
+            Self {
+                // Assumes upto 5-bytes for the first integer and assuming 1 byte for all subsequent integers.
+                //
+                // While this won't always be exact, it should be within a single power of 2 of the result size
+                // which will avoid many unnecessary re-allocations without overzealously over-allocatiing.
+                output: Vec::with_capacity(num_ints + 5),
+                prev: 0,
+            }
+        }
+
+        pub fn encode(&mut self, value: u32) {
+            let mut buffer = [0; 5];
+            let signed = value as i32 - self.prev as i32;
+            let zigzag = (signed << 1) ^ (signed >> 31);
+            let encoded = unsigned_varint::encode::u32(zigzag as u32, &mut buffer);
+            self.output.extend_from_slice(encoded);
+            self.prev = value;
+        }
+
+        pub fn finish(self) -> Vec<u8> {
+            self.output
+        }
+    }
+
+    pub struct U32ZigZagDecoder<'a> {
+        input: &'a [u8],
+        prev: u32,
+    }
+
+    impl<'a> U32ZigZagDecoder<'a> {
+        pub fn new(input: &'a [u8]) -> Self {
+            Self { input, prev: 0 }
+        }
+
+        pub fn decode_all(mut self) -> Option<Vec<u32>> {
+            let min_results = 1 + (self.input.len() / 10); // results will contain _at least_ one integer per 10 bytes
+            let mut results = Vec::with_capacity(min_results);
+            while !self.input.is_empty() {
+                results.push(self.decode()?);
+            }
+            Some(results)
+        }
+
+        #[inline]
+        pub fn decode(&mut self) -> Option<u32> {
+            let (head, tail) = unsigned_varint::decode::u32(self.input).ok()?;
+            let delta = (head as u32 >> 1) as i32 ^ -((head & 1) as i32);
+            let value = (self.prev as i32 + delta) as u32;
+            self.input = tail;
+            self.prev = value;
+            Some(value)
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn u32_varint_encoder_works() {
+            let inputs: [&[u32]; 9] = [
+                &[0].as_ref(),
+                &[0, 0, 0, 0, 0, 0, 0, 0, 0].as_ref(),
+                &[1].as_ref(),
+                &[1, 1, 1, 1, 1, 1, 1, 1, 1].as_ref(),
+                &[5].as_ref(),
+                &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].as_ref(),
+                &[u32::MIN].as_ref(),
+                &[u32::MAX].as_ref(),
+                &[u32::MAX, u32::MAX, u32::MAX].as_ref(),
+            ];
+            for values in &inputs {
+                let mut encoder = U32VarintEncoder::sized_for(inputs.len());
+                for n in *values {
+                    encoder.encode(*n);
+                }
+
+                // Compress the inputs
+                let encoded = encoder.finish();
+
+                // NOTE: Encoding of MAX-range values will cause compressed values to be large than expected
+                // if values.len() > 1 {
+                //     assert!(encoded.len() < std::mem::size_of::<u32>() * values.len());
+                // }
+
+                // Ensure that the decoded value matches the input
+                let decoded = U32VarintDecoder::new(&encoded).decode_all().unwrap();
+                assert_eq!(&decoded, values);
+            }
+        }
+
+        #[test]
+        fn u32_zigzag_encoder_works() {
+            let inputs: [&[u32]; 9] = [
+                &[0].as_ref(),
+                &[0, 0, 0, 0, 0, 0, 0, 0, 0].as_ref(),
+                &[1].as_ref(),
+                &[1, 1, 1, 1, 1, 1, 1, 1, 1].as_ref(),
+                &[5].as_ref(),
+                &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].as_ref(),
+                &[u32::MIN].as_ref(),
+                &[u32::MAX].as_ref(),
+                &[u32::MAX, u32::MAX, u32::MAX].as_ref(),
+            ];
+            for values in &inputs {
+                let mut encoder = U32ZigZagEncoder::sized_for(inputs.len());
+                for n in *values {
+                    encoder.encode(*n);
+                }
+
+                // Compress the inputs
+                let encoded = encoder.finish();
+                if values.len() > 1 {
+                    assert!(encoded.len() < std::mem::size_of::<u32>() * values.len());
+                }
+
+                // Ensure that the decoded value matches the input
+                let decoded = U32ZigZagDecoder::new(&encoded).decode_all().unwrap();
+                assert_eq!(&decoded, values);
+            }
+        }
     }
 }
